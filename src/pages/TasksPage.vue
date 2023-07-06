887175615abebc0dafc7d1b5d8a4189ac2b5736a
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-07-06 11:23:42 +0530

    diff between completed and not completed, also setup button press when show the task

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -8,12 +8,18 @@
                         <input type="text" class="form-control form-control-lg padding-right-lg"
                             placeholder="+ Add new task. Press enter to save." />
                     </div>
-                    <!-- List of tasks -->
-                    <div class="card mt-2">
-                        <ul class="list-group list-group-flush">
-                            <Task v-for="task in tasks" :task="task" :key="task.id" />
-                        </ul>
+                    <!-- List of uncompleted tasks -->
+                    <Tasks :tasks="uncompletedTasks" />
+                    <!-- show toggle button -->
+                    <div class="text-center my-3" v-show="showToggleCompletedBtn">
+                        <button class="btn btn-sm btn-secondary" 
+                            @click="showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show completed</span>
+                            <span v-else>Hide completed</span>
+                        </button>
                     </div>
+                    <!-- list of completed tasks -->
+                    <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks"/>
                 </div>
             </div>
         </div>
@@ -21,12 +27,23 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { allTasks } from "../http/task-api";
-import Task from "../components/tasks/Task.vue";
+import Tasks from "../components/tasks/Tasks.vue";
 const tasks = ref([])
 onMounted(async () => {
     const { data } = await allTasks()
     tasks.value = data.data
 })
+
+const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+
+const showToggleCompletedBtn = computed(
+    () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+)
+const completedTasksIsVisible = computed(
+    () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+)
+const showCompletedTasks = ref(false)
 </script>
